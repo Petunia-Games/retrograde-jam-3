c90866baf9d58759b3e3175566ffee8347449517
Daniel Ingram <petunia.games@mail.com>
2022-07-01 22:57:24 +0100

    Added player party stats

--- PlayerParty.gd
+++ PlayerParty.gd
@@ -1,5 +1,40 @@
 extends Node
 
+const NAME = "name"
+const HP_CURRENT = "hp_current"
+const HP_MAX = "hp_max"
+const LEVEL = "level"
+const EXP_CURRENT = "exp_current"
+const EXP_NEXT = "exp_next"
+
+
+var char_1: Dictionary = {
+	NAME:"char_1",
+	HP_CURRENT:120,
+	HP_MAX:120,
+	LEVEL:1,
+	EXP_CURRENT:0,
+	EXP_NEXT:100,
+}
+
+var char_2: Dictionary = {
+	NAME:"char_2",
+	HP_CURRENT:100,
+	HP_MAX:100,
+	LEVEL:1,
+	EXP_CURRENT:0,
+	EXP_NEXT:100,
+}
+
+var char_3: Dictionary = {
+	NAME:"char_3",
+	HP_CURRENT:150,
+	HP_MAX:150,
+	LEVEL:1,
+	EXP_CURRENT:0,
+	EXP_NEXT:100,
+}
+
 var current_party: Array = []
 
 
@@ -11,3 +46,5 @@ func remove_from_party(value) -> void:
 	current_party.remove(value)
 	
 
+func level_up_member(member) -> void:
+	pass

17e026d37d73ae1ce2147caa69d08f6474b0e99d
Daniel Ingram <petunia.games@mail.com>
2022-07-01 17:54:58 +0100

    Fixed timing for beat detection

--- Prototype/Battle/Battle.gd
+++ Prototype/Battle/Battle.gd
@@ -4,9 +4,13 @@ onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 onready var anim: AnimationPlayer = $AnimationPlayer
 onready var battle_timer: Timer = $BattleTimer
 
+var battle_queue: Array = []
+var decision_phase = false
+var action_phase = false
+
 var tempo = 120.0
 var timer = Timer.new()
-var countdown_duration: float = (60000.0 / tempo / 1000.0)
+var countdown_duration: float = 60000.0 / tempo / 1000.0
 
 
 func _ready() -> void:
@@ -16,11 +20,41 @@ func _ready() -> void:
 	add_child(timer)
 	battle_timer.start(2)
 	
+	
+func _process(delta: float) -> void:
+	if is_on_beat():	
+		$Label2.text = "NOW"
+		$ColorRect.visible = true
+	else:
+		$Label2.text = ""
+		$ColorRect.visible = false
+	get_input()
+
+func get_input() -> void:
+	if Input.is_action_just_pressed("confirm"):
+		if action_phase:
+			if is_on_beat():
+				$Label.text = "ON BEAT"
+			else:
+				$Label.text = "OFF BEAT"
+
+
+func is_on_beat() -> bool:
+	var upper = countdown_duration * 0.1
+	var lower = countdown_duration * 0.6
+	var time_left = timer.time_left
+	
+	if time_left <= upper or time_left >= lower:
+		return true
+	return false
+
 
 func _on_battle_timer_timeout() -> void:
+	action_phase = true
 	timer.start(countdown_duration)
 	audio_player.play()
 
 
 func _on_timer_timeout() -> void:
-	anim.play("FlashWhite")
+	pass
+	#anim.play("FlashWhite")

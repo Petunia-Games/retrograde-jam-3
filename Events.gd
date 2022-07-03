761d5c6244ff5ee58bf19b9f8fb25408cd325df8
Daniel Ingram <petunia.games@mail.com>
2022-07-03 20:49:18 +0100

    Added Events bus

---
+++ Events.gd
@@ -0,0 +1,7 @@
+extends Node
+
+# Global bus used to connect signals between nodes
+
+signal battle_started(msg)
+signal battle_finished(msg)
+
--- Game.gd
+++ Game.gd
@@ -1,6 +1,11 @@
 extends Node
 
+
+var battle_scene = preload("res://Battle/Battle.tscn")
+
+
 func _ready() -> void:
+	begin_battle()
 	new_game()
 	
 	
@@ -8,3 +13,8 @@ func new_game() -> void:
 	# Set values to their defaults
 	# Start intro
 	pass
+
+
+func begin_battle() -> void:
+	var battle = battle_scene.instance()
+	add_child(battle)

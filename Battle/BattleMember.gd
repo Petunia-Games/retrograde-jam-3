0aa1be3afe45c6d412b16b99629294f11084214c
Daniel Ingram <petunia.games@mail.com>
2022-07-04 00:05:36 +0100

    Changed battle member prototype

--- Battle/BattleMember.gd
+++ Battle/BattleMember.gd
@@ -1,4 +1,16 @@
 class_name BattleMember
 extends Control
 
-var member_name: String = ""
+export(String) var member_name: String = ""
+export(Texture) var sprite
+export(bool) var is_enemy = false
+export(bool) var stats_known = false
+
+export(int) var level: int = 0
+export(int) var experience: int = 0
+export(int) var current_hp: int = 0
+export(int) var max_hp: int = 0
+export(int) var strength: int = 0
+export(int) var speed: int = 0
+export(int) var sorcery: int = 0
+export(int) var defense: int = 0

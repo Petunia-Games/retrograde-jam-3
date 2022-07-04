93be2df115f2a0d26b84144c9b8e67ccfaa27800
Daniel Ingram <petunia.games@mail.com>
2022-07-04 02:12:42 +0100

    Added Map scene

--- Game.gd
+++ Game.gd
@@ -1,13 +1,13 @@
 extends Node
 
 var battle_scene = preload("res://Battle/Battle.tscn")
-
+var map_scene = preload("res://Battle/Map/Map.tscn")
 
 func _ready() -> void:
 	Globals.encounter_id = 0
 	Globals.field_id = 0
 	Globals.current_party = [0,1,2]
-	begin_battle()
+	#begin_battle()
 	
 	
 func new_game() -> void:

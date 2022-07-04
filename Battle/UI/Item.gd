9ea459a384979dee42967478f4222d30abdc33c4
Daniel Ingram <petunia.games@mail.com>
2022-07-04 20:18:39 +0100

    Added Item global data

--- Battle/UI/Item.gd
+++ Battle/UI/Item.gd
@@ -1,4 +1,17 @@
 extends HBoxContainer
 
-func set_data() -> void:
+onready var pointer: TextureRect = $Pointer
+onready var name_label: Label = $ItemNameLabel
+onready var quantity_label: Label = $QuantityLabel
+
+
+func set_data(item_data) -> void:
+	pass
+	
+	
+func set_selected() -> void:
+	pass
+	
+	
+func set_deselected() -> void:
 	pass
---
+++ Items.gd
@@ -0,0 +1,16 @@
+extends Node
+
+enum {
+	POTION,
+	
+}
+
+const NAME = "name"
+const DESCRIPTION = "description"
+
+var id: Dictionary = {
+	str(POTION):{
+		NAME:"Potion",
+		DESCRIPTION:"Heals 100HP."
+	}
+}

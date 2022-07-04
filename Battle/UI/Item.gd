b3ab97644c672d19f814562615fac88493b7729e
Daniel Ingram <petunia.games@mail.com>
2022-07-04 21:41:23 +0100

    Added ability to add items to the list

--- Battle/UI/Item.gd
+++ Battle/UI/Item.gd
@@ -5,13 +5,17 @@ onready var name_label: Label = $ItemNameLabel
 onready var quantity_label: Label = $QuantityLabel
 
 
+func _ready() -> void:
+	pointer.self_modulate.a = 0.0
+
+
 func set_data(item_data) -> void:
 	pass
 	
 	
 func set_selected() -> void:
-	pass
+	pointer.self_modulate.a = 1.0
 	
 	
 func set_deselected() -> void:
-	pass
+	pointer.self_modulate.a = 0.0
--- Battle/UI/ItemList.gd
+++ Battle/UI/ItemList.gd
@@ -1,5 +1,7 @@
 extends GridContainer
 
+var item_scene = preload("res://Battle/UI/Item.tscn")
+
 
 func process_input() -> void:
 	if Input.is_action_just_pressed("up"):
@@ -20,5 +22,18 @@ func process_input() -> void:
 		Events.emit_signal("battle_member_changed")
 
 
+func add_item_to_list(item_data) -> void:
+	var item = item_scene.instance()
+	add_child(item)
+	item.set_data(item_data)
+
+
 func populate_list(item_data) -> void:
-	pass
+	clear_list()
+	for item in item_data:
+		add_item_to_list(item)
+
+
+func clear_list() -> void:
+	for child in get_children():
+		child.free()

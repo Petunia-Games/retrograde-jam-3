cfb1b732eb475416a04abfa0892282285aa9ab38
Daniel Ingram <petunia.games@mail.com>
2022-07-04 07:23:28 +0100

    Added spell data

--- Battle/UI/SorceryList.gd
+++ Battle/UI/SorceryList.gd
@@ -1,7 +1,20 @@
 extends GridContainer
 
-var spell = preload("res://Battle/UI/Spell.tscn")
+var spell_scene = preload("res://Battle/UI/Spell.tscn")
 
 
-func populate_list(spell_data) -> void:
+func process_input() -> void:
 	pass
+
+
+func add_spell_to_list(spell_data) -> void:
+	var spell = spell_scene.instance()
+	add_child(spell)
+	spell.set_data(spell_data)
+
+
+func populate_list(member_data) -> void:
+	for spell in member_data[PlayerParty.SPELLS]:
+		add_spell_to_list(spell)
+
+	get_child(0).set_selected()
--- Battle/UI/Spell.gd
+++ Battle/UI/Spell.gd
@@ -1,2 +1,24 @@
 extends HBoxContainer
 
+onready var pointer: TextureRect = $Pointer
+onready var spell_name_label: Label = $SpellNameLabel
+onready var spell_uses_remaining: Label = $SpellUsesLabel
+onready var spell_uses_max: Label = $SpellMaxUsesLabel
+
+
+func _ready() -> void:
+	pointer.self_modulate.a = 0.0
+
+func set_data(spell_data: Dictionary) -> void:
+	var spell_id = spell_data.keys()
+	spell_name_label.text = Spells.id[spell_id[0]][Spells.NAME]
+	spell_uses_remaining.text = str(spell_data[spell_id[0]][PlayerParty.SPELL_POINTS_CURRENT])
+	spell_uses_max.text = str(spell_data[spell_id[0]][PlayerParty.SPELL_POINTS_MAX])
+
+
+func set_selected() -> void:
+	pointer.self_modulate.a = 1.0
+	
+	
+func set_deselected() -> void:
+	pointer.self_modulate.a = 0.0

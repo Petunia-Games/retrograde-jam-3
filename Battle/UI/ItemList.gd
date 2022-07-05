9df6dabd0a80243542bb8d1497a33bb6b46bae11
Daniel Ingram <petunia.games@mail.com>
2022-07-05 02:11:18 +0100

    Added ability to select items to use

--- Battle/UI/ItemList.gd
+++ Battle/UI/ItemList.gd
@@ -2,22 +2,26 @@ extends GridContainer
 
 var item_scene = preload("res://Battle/UI/Item.tscn")
 var selected_item_index = 0
+var current_column = 0
+
 
 func process_input() -> void:
 	if Input.is_action_just_pressed("up"):
-		pass
+		select_previous_item_in_column()
 	elif Input.is_action_just_pressed("down"):
-		pass
+		select_next_item_in_column()
 	elif Input.is_action_just_pressed("left"):
-		pass
+		change_column()
 	elif Input.is_action_just_pressed("right"):
-		pass
+		change_column()
 	
 	if Input.is_action_just_pressed("confirm"):
-		pass
+		Events.emit_signal("battle_item_selected", get_child(selected_item_index))
 	elif Input.is_action_just_pressed("cancel"):
+		get_child(selected_item_index).set_deselected()
 		Events.emit_signal("battle_submenu_cancelled")
 	elif Input.is_action_just_pressed("select"):
+		get_child(selected_item_index).set_deselected()
 		BattleGlobals.set_active_party_member()
 		Events.emit_signal("battle_member_changed")
 
@@ -29,6 +33,8 @@ func add_item_to_list(item_data) -> void:
 
 
 func populate_list(item_data: Dictionary) -> void:
+	selected_item_index = 0
+	current_column = 0
 	clear_list()
 	for item in item_data:
 		var new_data = {str(item):PlayerParty.items[item]}
@@ -39,3 +45,64 @@ func populate_list(item_data: Dictionary) -> void:
 func clear_list() -> void:
 	for child in get_children():
 		child.free()
+
+
+func select_previous_item_in_column() -> void:
+	var item_count = get_child_count()
+	get_child(selected_item_index).set_deselected()
+	if selected_item_index == 0:
+		if item_count >= 2:
+			if item_count % 2 == 0:
+				selected_item_index = item_count - 2
+			else:
+				selected_item_index = item_count - 1
+		else:
+			selected_item_index = 0
+	elif selected_item_index == 1:
+		if item_count >= 4:
+			if item_count % 2 == 0:
+				selected_item_index = item_count - 1
+			else:
+				selected_item_index = item_count - 2
+		else:
+			selected_item_index = 1
+	else:
+		selected_item_index -= 2
+		
+	get_child(selected_item_index).set_selected()
+		
+	
+func select_next_item_in_column() -> void:
+	var item_count = get_child_count()
+	get_child(selected_item_index).set_deselected()
+	if current_column == 0:
+		if selected_item_index + 2 > item_count - 1:
+			selected_item_index = 0
+		elif selected_item_index + 2 == item_count - 1:
+			selected_item_index = item_count - 1
+		else:
+			selected_item_index += 2
+	if current_column == 1:
+		if selected_item_index + 2 > item_count - 1:
+			selected_item_index = 1
+		elif selected_item_index + 2 == item_count - 1:
+			selected_item_index = item_count - 1
+		else:
+			selected_item_index += 2
+			
+	get_child(selected_item_index).set_selected()
+
+
+func change_column() -> void:
+	get_child(selected_item_index).set_deselected()
+	var item_count = get_child_count()
+	
+	if current_column == 0:
+		if item_count >= selected_item_index + 2:
+			current_column = 1
+			selected_item_index += 1
+	else:
+		current_column = 0
+		selected_item_index -= 1
+			
+	get_child(selected_item_index).set_selected()
